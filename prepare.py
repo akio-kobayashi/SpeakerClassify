adf163554ed4f391fdc805b47d2f0cbae85796ee
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2024-10-07 14:32:34 +0900

    add file

---
+++ prepare.py
@@ -0,0 +1,15 @@
+import os, sys
+import glob
+import pandas as pd
+
+root='/content/drive/Shareddrive/専門演習I/後期前半/SpeechData/'
+data={'path':[], 'data_type':[], 'speaker': [] }
+for file in glob.glob(os.path.join(root, '*/*wav')):
+    print(file)
+    speaker = os.path.dirname(file)
+    #data['path'].append(os.path.join(root, file))
+    #data['data_type'].append('train')
+    #data['speaker'].append(speaker)
+   
+#df = pd.DataFrame.from_dict(data, orient='columns')
+#df.to_csv('data.csv', index=False)
