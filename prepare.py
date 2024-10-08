67383838542745067ab43c682ffd718b8ed8bd19
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2024-10-08 12:20:35 +0900

    fix bugs

--- prepare.py
+++ prepare.py
@@ -2,14 +2,23 @@ import os, sys
 import glob
 import pandas as pd
 
+valid=['11','12','13','14','15']
+test=['16','17','18','19','20']
+
 root='/content/drive/Shareddrives/専門演習I/後期前半/SpeechData/'
 data={'path':[], 'data_type':[], 'speaker': [] }
-for file in glob.glob(os.path.join(root, '*/*wav')):
-    print(file)
-    speaker = os.path.dirname(file)
-    #data['path'].append(os.path.join(root, file))
-    #data['data_type'].append('train')
-    #data['speaker'].append(speaker)
+for path in glob.glob(os.path.join(root, '*/*wav')):
+    print(path)
+    speaker = os.path.dirname(path)
+    data['path'].append(os.path.join(root, file))
+    num=os.path.splitext(os.path.basename(path))[0]).split('_')[1]
+    if num in valid:
+        data['data_type'].append('valid')
+    elif num in test:
+        data['data_type'].append('eval')
+    else:
+        data['data_type'].append('train')
+    data['speaker'].append(speaker)
    
-#df = pd.DataFrame.from_dict(data, orient='columns')
-#df.to_csv('data.csv', index=False)
+df = pd.DataFrame.from_dict(data, orient='columns')
+df.to_csv(os.path.join(root, 'data.csv'), index=False)
