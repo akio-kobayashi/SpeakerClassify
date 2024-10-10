0f9e4de1688b208e69848feb10de452437321ece
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2024-10-10 12:25:27 +0900

    fix bugs

--- predict.py
+++ predict.py
@@ -32,7 +32,7 @@ def predict(config:dict, data_type="eval", sample_rate=16000):
         df = pd.read_csv(config['csv'])
         for idx, row in df.query('data_type==@data_type').iterrows():
             wave, sr = torchaudio.load(row['path'])
-            std, mean = torch.std_mean(wave)
+            std, mean = torch.std_mean(wave, dim=-1)
             wave = (wave - mean)/std
             spec = torch.log10(transform(wave) + 1.e-9)
             #std, mean = torch.std_mean(spec)
