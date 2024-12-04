ef5c878230f4dd4de296ed63039ecceffff39f77
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2024-12-04 19:57:37 +0900

    fix bugs

--- predict.py
+++ predict.py
@@ -33,8 +33,8 @@ def predict(config:dict, data_type="eval", sample_rate=16000):
         for idx, row in df.query('data_type==@data_type').iterrows():
             wave, sr = torchaudio.load(row['path'])
             wave = wave[0, :].unsqueeze(dim=0)
-            if sr != self.sample_rate:
-                resampler = torchaudio.transforms.Resample(sr, self.sample_rate, dtype=wave.dtype)
+            if sr != 16_000:
+                resampler = torchaudio.transforms.Resample(sr, 16_000, dtype=wave.dtype)
                 wave = resampler(wave)
             std, mean = torch.std_mean(wave, dim=-1)
             wave = (wave - mean)/std
